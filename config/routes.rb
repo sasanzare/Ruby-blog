f5c55352824144eb9c4fef6f06b689ee0eae016b
sasanzare <sasanzare75@gmail.com>
2022-04-17 17:23:54 +0430

    add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root "articles#index"
+  get 'home' , to: "pages#home"
+  get 'contact', to: "pages#contact"
+  get 'about', to: "pages#about"
   get "/articles", to: "articles#index"
+  root "pages#home"
 end
